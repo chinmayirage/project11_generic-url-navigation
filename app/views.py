09fc8c9c56378b5ee62204bef5d71bca31772f98
chinmayirage <chinmayirage37@gmail.com>
2023-06-04 22:05:44 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def chinnu(request):
+    return render(request,'chinnu.html')
+def poornima(request):
+    return render(request,'poornima.html')
